9d60b08b41770cc45791b0ff23f4539ea92f61ad
GitJuski <juuso.vainikka@outlook.com>
2024-10-19 13:51:56 +0300

    Debug and fix, add comments, add list check in function random

--- djangosite/static/weeklyfoodlist/script.js
+++ djangosite/static/weeklyfoodlist/script.js
@@ -5,7 +5,7 @@ class FoodLists {
         this.other = [];
     }
 
-    addFood(food) {
+    addFood(food) {     // Method for adding food to the FoodLists attributes (fish list, soups, list, other list) If object.category == 'Fish' it goes to the fish list etc.
 
             switch (food.category) {
                 case 'Fish':
@@ -20,45 +20,59 @@ class FoodLists {
             }
         }
 
-    getFishList() {
+    getFishList() {     // Method for getting the fish list
         return this.fish;
     }
 
-    getSoupList() {
-        return this.soups
+    getSoupList() {     // Method for getting the soups list
+        return this.soups;
     }
 
-    getOtherList() {
-        return this.other
+    getOtherList() {    // Method for getting the other lists
+        return this.other;
     }
 }
 
-let foodLists = new FoodLists();
+const foodLists = new FoodLists();    // Creates a new FoodLists object
 
-async function getFoods() {
+async function getFoods() {     // API call function (Gets called when the frontpage is loaded)
     const url = 'http://192.168.69.100/api/food/';
 
     try {
         const response = await fetch(url);
         if (!response.ok) {
-            throw new Error(`Response status: ${response.status}`)
+            throw new Error(`Response status: ${response.status}`);
         }
 
         const json = await response.json();
-        json.forEach(food => foodLists.addFood(food));
+        json.forEach(food => foodLists.addFood(food));      // Uses addFood() method to add JSON objects to the lists
     } catch (error) {
         console.log(error.message);
     }
 }
 
 
-function random(list) {
+function random(list, weeksList) {     // A function to return a random object from a list
 
-    return list[Math.floor(Math.random() * list.length)];
+    const max = 10;     // MAX amount for the while loop to try getting an unique value. This dodges infinite loops from forming
+    let attempts = 0;
+
+    while (attempts < max) {
+
+        const chosen = list[Math.floor(Math.random() * list.length)];
+
+        if (!weeksList.includes(chosen)) {      // If the object isn't in the weekslist then add it
+            return chosen;
+        }
+
+        attempts ++;
+    }
+
+    throw new Error('All the possible values have been chosen');
 }
 
 
-function shuffle(array) {
+function shuffle(array) {   // A function to shuffle the array and return the array
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
@@ -67,7 +81,7 @@ function shuffle(array) {
 }
 
 
-function refresh() {
+function refresh() {    // Function to show/refresh the list on the frontpage
 
     const list = document.getElementById("list");
 
@@ -76,14 +90,14 @@ function refresh() {
     const soups = foodLists.getSoupList();
     const other = foodLists.getOtherList();
 
-    weeksList.push(random(fish));
-    weeksList.push(random(soups));
+    weeksList.push(random(fish, weeksList));   // Calls the random() function and gives the function the fish list and adds the return value to the weeksList list
+    weeksList.push(random(soups, weeksList));  // Calls the random() function and gives the function the soups list and adds the return value to the weeksList list
 
     for (let i = 2; i < 7; i++) {
-        weeksList.push(random(other));
+        weeksList.push(random(other, weeksList));  // Calls the random() function and gives the function the other list and adds the return value to the weeksList list five times
     }
 
-    weeksList = shuffle(weeksList);
+    weeksList = shuffle(weeksList);     // Calls the shuffle() funtion and gives it the weekslist list
 
 
     list.innerHTML = `
@@ -99,4 +113,4 @@ function refresh() {
   `;
 }
 
-window.onload = getFoods;
+window.onload = getFoods;   // When the frontpage is loaded this calls the getFoods function to fetch the food data
